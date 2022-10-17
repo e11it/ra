8849a5a527b66cb04fff14f88ff8cce83ea3a180
github-action-benchmark <github@users.noreply.github.com>
2022-10-17 19:17:54 +0000

    add Go Benchmark (go) benchmark result for 0ae0227702452bb8e7583c0be4c9f971dc89022c

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1666034274778,
+  "repoUrl": "https://github.com/e11it/ra",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "im@e11it.ru",
+            "name": "e11it",
+            "username": "e11it"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0ae0227702452bb8e7583c0be4c9f971dc89022c",
+          "message": "Update main_test.go",
+          "timestamp": "2022-10-17T22:14:10+03:00",
+          "tree_id": "af1e8be67cc819207989957c65a689a254e72108",
+          "url": "https://github.com/e11it/ra/commit/0ae0227702452bb8e7583c0be4c9f971dc89022c"
+        },
+        "date": 1666034274020,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkAuthRequest",
+            "value": 17828,
+            "unit": "ns/op\t    4841 B/op\t      56 allocs/op",
+            "extra": "66984 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
